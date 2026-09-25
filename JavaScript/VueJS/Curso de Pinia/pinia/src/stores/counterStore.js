a147e0509ca661dcb2e3f07fd6f5cdbd998ebe14
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2026-09-25 10:03:07 -0300

    Revisão o conteúdo 25/09/2026

--- JavaScript/VueJS/Curso de Pinia/pinia/src/stores/counterStore.js
+++ JavaScript/VueJS/Curso de Pinia/pinia/src/stores/counterStore.js
@@ -3,39 +3,64 @@ import { defineStore } from 'pinia'
 export const contando = defineStore('contar', {
 
   // Estado.
-  state(){
-    return{
+  state() {
+
+    return {
+
       contador: 0,
-      mensagem: '',
+
+      mensagem: ''
+
     }
+
   },
 
   // Ações.
-  actions:{
-    incrementando(){
-      if(this.contador >= 10){
+  actions: {
+
+    incrementando() {
+
+      if(this.contador >= 10) {
+
         this.mensagem = 'O contador não pode ser maior que 10!'
-      } else if (this.contador < 10){
+
+      } else if (this.contador < 10) {
+
         this.contador++
+
         this.mensagem = ''
+
       }
+
     },
 
-    decrementando(){
-      if(this.contador < 1){
+    decrementando() {
+
+      if(this.contador < 1) {
+
         this.mensagem = 'O contador não pode ser menor que 0!'
+
       } else {
+
         this.contador--
+
         this.mensagem = ''
+
       }
+
     }
+
   },
 
   // Métodos de Obtenção.
-  getters:{
-    exibirContagem(state){
+  getters: {
+
+    exibirContagem(state) {
+
       return `O valor do contador é: ${state.contador}!`
+
     }
+
   }
 
 })
